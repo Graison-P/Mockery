cc6db7c00b468f8be4b3ee85b83b3aef94c6fe98
Graison Parkhurst <119984713+Graison-P@users.noreply.github.com>
2026-03-04 16:43:00 -0600

    added the stuff

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,21 @@
 #include <Geode/Geode.hpp>
+#include <Geode/modify/PlayerObject.hpp>
+#include <cstdlib> // For rand()
 
 using namespace geode::prelude;
 
+class $modify(PlayerObject) {
+    void playerDestroyed(bool p0) {
+        // call player death
+        PlayerObject::playerDestroyed(p0);
+
+        // pick random num 1-5 for sound
+        int soundID = std::rand() % 5 + 1;
+        
+        // construct file name
+        std::string soundFile = std::to_string(soundID) + ".ogg"_spr;
+
+        // play file into engine
+        FMODAudioEngine::sharedEngine()->playEffect(soundFile);
+    }
+};
